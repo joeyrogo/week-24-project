00401060ab809295b47e5ecd161ed71a63183b8f
joeyrogo <67221823+joeyrogo@users.noreply.github.com>
2022-02-04 16:39:33 -0500

    Update bucket.tf

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -1,9 +1,7 @@
 resource "aws_s3_bucket" "demos3" {
     bucket = "${var.bucket_name}" 
     acl = "${var.acl_value}"   
-    versioning {
-    enabled = true
-  }
+  
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
